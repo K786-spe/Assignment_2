b19db2f34fb0f6182c47e972eac9fec322676292
K786-spe <raimuhammadkaleem@gmail.com>
2025-04-16 11:47:25 +0500

    Add files via upload

---
+++ Sourse6.cpp
@@ -0,0 +1,33 @@
+//#include <iostream>
+//using namespace std;
+//int reverse(int a[10]);
+//
+//int main() {
+//    int b[10];
+//    cout << "Entre arr: " << endl;
+//    for (int i = 0; i <= 9; i++)
+//    {
+//        cin >> b[i];
+//    }
+//    reverse(b);
+//    cout << "reverse arr is: ";
+//    for (int i = 0; i <= 9; i++)
+//    {
+//        cout << b[i];
+//    }
+//    cout << endl;
+//    system("pause");
+//    return 0;
+//}
+//int reverse(int a[10]) {
+//    int b[10];
+//    int j = 9;
+//    for (int i = 0; i < 10; i++) {
+//        b[i] = a[j];
+//        j--;
+//    }
+//    for (int i = 0; i < 10; i++) {
+//        a[i] = b[i];
+//    }
+//    return 0;
+//}
